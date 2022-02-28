b1537542f8d98626b82f65c61958cb4013c4e035
Minhhk <alszn2222@gmail.com>
2022-03-01 00:28:31 +0900

    swiper
    pagination: { el: ,clickable:}
    
    navigation: {prevEl, nextEl:}

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ searchInputEl.addEventListener('blur', function() {
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', _.throttle( function() {
+window.addEventListener('scroll', _.throttle( function() { // _.throttle(함수, 시간 == ms) 
   if (scrollY > 500) { //배지 숨기기 //badgeEl.style.display = 'none';
     //gsap.to(요소, 지속시간, 옵션);
     gsap.to(badgeEl, 0.6, {
@@ -31,7 +31,7 @@ window.addEventListener('scroll', _.throttle( function() {
     })
   }
 }, 300));
-// _.throttle(함수, 시간 == ms) 
+
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
@@ -52,3 +52,22 @@ new Swiper('.notice-line .swiper', {
   autoplay: true, // 자동 재생 여부
   loop: true // 반복 재생 여부
 })
+
+new Swiper('.promotion .swiper-container',{
+  //기본값direction: 'horizontal',
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable: true, //사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next',
+  }
+});
