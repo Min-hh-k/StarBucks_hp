39b0d4f07d8a34280991398a0fa6549a4c27770a
Minhhk <alszn2222@gmail.com>
2022-03-07 23:17:32 +0900

    up

--- js/youtube.js
+++ js/youtube.js
@@ -2,7 +2,7 @@
      // 2. This code loads the IFrame Player API code asynchronously.
       var tag = document.createElement('script');
 
-      tag.src = "https://www.youtube.com/iframe_api";
+      tag.src = "https://www.youtube.com/iframe_api";AIzaSyAC2AHCh03GZMs_OguFpkihpZgBYAh8AMw
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
@@ -17,6 +17,7 @@
             autoplay: true, //자동 재생
             loop: true, //반복 재생 유무
             playlist: 'An6LvWQuj_8', // 반복재생 할 경우 loop 다음에 playlist: id 추가
+            key: AIzaSyAC2AHCh03GZMs_OguFpkihpZgBYAh8AMw
           },
           events: { //영상이 준비 되었을 때의 효과
             onReady: function (event) {
